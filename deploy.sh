fe292fa3125ff87adcce2e7153254f04c25be9e3
Pinkeshkr <pinkeshkr@icloud.com>
2023-07-14 02:40:47 +0530

    Revert "Updated"
    This reverts commit 391b954670cf0f2534e9cda9721a7f371089e092.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 Adding again in development branch..
 
+=======
+>>>>>>> 6eee039 (Revert "Updated")
 Added the new scripts for the deployment...
 This file contains the deployment scripts.
